50b3456a2d82aa811e5eefdbd235d848c1575ce7
juyeon707 <juyeon7007@gmail.com>
2026-04-17 23:12:00 +0900

    feat: CategoryForm DTO 추가

---
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -0,0 +1,30 @@
+package kr.ac.hansung.cse.model;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class CategoryForm {
+
+    private Long id;
+
+    @NotBlank(message = "카테고리 이름을 입력하세요")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
+    private String name;
+
+    public Category toEntity() {
+        return new Category(this.name);
+    }
+
+    public static CategoryForm from(Category category) {
+        CategoryForm form = new CategoryForm();
+        form.id = category.getId();
+        form.name = category.getName();
+        return form;
+    }
+}
