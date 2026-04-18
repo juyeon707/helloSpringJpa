b0ecee71c5bc456981babbe4c62c1efb557b49ac
juyeon707 <juyeon7007@gmail.com>
2026-04-18 13:58:24 +0900

    feat: CategoryController 내 카테고리 전체 조회 메서드 추가

---
+++ src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package kr.ac.hansung.cse.controller;
+
+import jakarta.validation.Valid;
+import kr.ac.hansung.cse.exception.DuplicateCategoryException;
+import kr.ac.hansung.cse.model.Category;
+import kr.ac.hansung.cse.model.CategoryForm;
+import kr.ac.hansung.cse.service.CategoryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) { this.categoryService = categoryService; }
+
+    @GetMapping // GET /categories
+    public String listCategories(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "categoryList";
+    }
+
+}
